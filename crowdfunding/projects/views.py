e2f403a3469681830a7f526d69d4e9add3ff8f7b
qambatali <q.ambatali@gmail.com>
2021-09-14 20:27:55 +1000

    Update views.py

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -20,6 +20,7 @@ class PledgeList(APIView):
                 serializer.data,
                 status=status.HTTP_201_CREATED
             )
+            
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
