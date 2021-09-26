245a6116c9dab05c3a6c30ed3c78d2ee148cc56b
qambatali <q.ambatali@gmail.com>
2021-09-26 14:52:02 +1000

    Added feature to delete project

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -84,4 +84,14 @@ class ProjectDetail(APIView):
         )
         if serializer.is_valid():
             serializer.save()
-            
+
+    def delete(self, request, pk):
+        project = self.get_object(pk)
+        project.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+        if serializer.is_valid():
+            serializer.save()
+    
